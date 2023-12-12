0efcee6d8070e182fbf5ee11ad3b242d229a25dd
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2023-12-12 17:45:59 +0900

    :zap: CICD : deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-EXIST_NGINX=$(docker ps | grep front-web)
+EXIST_NGINX=$(docker ps | grep readbird)
+
 if [ -z "$EXIST_NGINX" ]; then 
     docker-compose -p readbird -f /home/ec2-user/deploy/conf.d/docker-compose.yml down
 fi 

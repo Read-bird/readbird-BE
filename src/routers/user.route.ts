d54bfb1ccfbe07a7c25ea8b912e97263b6b00b95
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2023-12-20 02:20:13 +0900

    :sparkles: Feat : 기록 초기화 - 모든 플랜 일괄 삭제 구현
    - 유저의 모든 플랜을 삭제합니다.
    - status를 delete로 변경합니다.
    
    Issues : #36

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import userController from "../architecture/controllers/user.controller";
 import refresh from "../jwt/refresh";
+import authJWT from "../jwt/authJWT";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", userController.signInKakao);
 userRouter.post("/token", refresh);
 userRouter.post("/login-guest", userController.signInGuest);
+userRouter.delete("/plan/delete", authJWT, userController.deleteAllPlan);
 
 export default userRouter;

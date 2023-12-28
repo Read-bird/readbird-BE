5eb365584923d385f060a11ac94ae4980db145c7
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2023-12-28 21:44:44 +0900

    :bug: Fix : target util 수정

--- src/util/getPlanTarget.ts
+++ src/util/getPlanTarget.ts
@@ -1,19 +1,24 @@
 import getDateFormat from "./setDateFormat";
 
 const getPlanTarget = (
-    endDate: Date,
+    endDate: Date | any,
     totalPage: number,
     currentPage: number,
 ) => {
     const today: any = new Date(getDateFormat(new Date()));
-    const masDate: any = new Date(endDate);
 
-    if (today === masDate) return totalPage - currentPage;
-
-    return Math.floor(
-        (totalPage - currentPage) /
-            Math.floor((masDate - today) / (1000 * 60 * 60 * 24)),
-    );
+    if (
+        today.getDate() === endDate.getDate() ||
+        today.getMonth() === endDate.getMonth() ||
+        today.getYear() === endDate.getYear()
+    ) {
+        return totalPage - currentPage;
+    } else {
+        return Math.floor(
+            (totalPage - currentPage) /
+                Math.floor((endDate - today) / (1000 * 60 * 60 * 24)),
+        );
+    }
 };
 
 export default getPlanTarget;

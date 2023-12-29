8a3b529608dc048dfe9b6e1ac271d32edb5eb6dd
seungE95 <ewotmd95@naver.com>
2023-12-30 07:27:06 +0900

    :bug: Fix: redis config 수정

--- src/redis.ts
+++ src/redis.ts
@@ -2,16 +2,22 @@ require("dotenv").config();
 import { createClient } from "redis";
 
 const redisClient = createClient({
-    url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
     legacyMode: true,
 });
+// const redisClient = createClient({
+//     url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+//     legacyMode: true,
+// });
 redisClient.on("connect", () => {
     console.info("Redis connected!");
 });
-redisClient.on("error", (err) => {
-    console.error("Redis Client Error", err);
-});
-redisClient.connect().then();
+// redisClient.on("error", (err) => {
+//     console.error("Redis Client Error", err);
+// });
+// redisClient.connect().then();
+redisClient.connect().catch(console.error);
 const redisCli = redisClient.v4;
 
 export default {

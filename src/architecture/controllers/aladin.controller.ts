b34611be9c92e3160a176e0ec4d7b6f331b70953
seungE95 <ewotmd95@naver.com>
2023-12-30 16:04:45 +0900

    :memo: docs: ttbkey 변수명 대문자로 수정

--- src/architecture/controllers/aladin.controller.ts
+++ src/architecture/controllers/aladin.controller.ts
@@ -4,90 +4,8 @@ import axios from "axios";
 import aladinRepository from "../repositories/aladin.repository";
 
 //알라딘 api 호출을 위한 개인키
-const ttbkey: String | undefined = process.env.ttbkey;
+const TTBKEY: String | undefined = process.env.TTBKEY;
 
-// async function monthlyFunction() {
-//     const { data } = await axios.get(
-//         "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=" +
-//             ttbkey +
-//             "&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101",
-//     );
-// }
-
-// cron.schedule("0 0 1 * *", () => {
-//     monthlyFunction();
-// });
-
-// const bookList: any = async () => {
-//     const { data } = await axios.get(
-//         "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=" +
-//             ttbkey +
-//             "&Query=aladdin&QueryType=Title&MaxResults=30&start=1&SearchTarget=Book&output=js&Version=20131101",
-//     );
-//     console.log("\nbookList::: " + JSON.stringify(data.item));
-//     return data.item;
-// };
-
-const getBookList = async (req: Request, res: Response, next: NextFunction) => {
-    //도서 리스트를 담는 데이터
-    const list: any = [];
-    let bList: any = [];
-    try {
-        for (let start = 1; start <= 1; start++) {
-            //도서 리스트, 불러온 데이터: 호출 한번에 출력 개수 30개
-
-            let {
-                data: { item },
-                //data
-            } = await axios.get(
-                "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=" +
-                    ttbkey +
-                    "&Query=all&QueryType=Title&MaxResults=50&start=" +
-                    start +
-                    "&SearchTarget=Book&output=js&Version=20131101",
-            );
-            list.push(item);
-        }
-
-        for (const sublist of list) {
-            for (const book of sublist) {
-                let { data } = await axios.get(
-                    "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=" +
-                        ttbkey +
-                        "&ItemId=" +
-                        book.isbn +
-                        "&output=JS",
-                );
-
-                //data가 String값으로 반환
-                let pageStr = data.slice(
-                    data.indexOf("itemPage") + 10,
-                    data.indexOf("itemPage") + 14,
-                );
-
-                let totalPage = pageStr.split(",").filter(Boolean);
-                if (<number>totalPage > 1) {
-                    const obj = {
-                        title: book.title,
-                        author: book.author,
-                        pubDate: book.pubDate,
-                        description: book.description,
-                        isbn: book.isbn,
-                        coverImage: book.cover,
-                        publisher: book.publisher,
-                        totalPage: totalPage.join(", "),
-                    };
-                    bList.push(obj);
-                }
-            }
-        }
-        let result = aladinRepository.bookUpload(bList);
-
-        return res.status(200).json({ result });
-    } catch (error) {
-        next(error);
-    }
-};
 const popularBook = async (req: Request, res: Response, next: NextFunction) => {
     //도서 리스트를 담는 데이터
     const list: any = [];
@@ -119,7 +37,7 @@ const popularBook = async (req: Request, res: Response, next: NextFunction) => {
             data: { item },
         } = await axios.get(
             "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=" +
-                ttbkey +
+                TTBKEY +
                 "&QueryType=Bestseller&MaxResults=10&start=1&Cover=Big&SearchTarget=Book&output=js&Version=20131101",
         );
 
@@ -127,7 +45,7 @@ const popularBook = async (req: Request, res: Response, next: NextFunction) => {
         for (const sublist of item) {
             const { data: ItemLookUpData } = await axios.get(
                 "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=" +
-                    ttbkey +
+                    TTBKEY +
                     "&ItemId=" +
                     sublist.isbn +
                     "&output=JS",
@@ -170,6 +88,5 @@ const popularBook = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export default {
-    getBookList,
     popularBook,
 };

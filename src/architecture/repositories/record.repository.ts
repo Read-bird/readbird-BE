dbf528b43fc20a2ae0ee223d2db2d9366f6f315a
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2024-01-04 09:54:56 +0900

    :bug: Fix : 완독 실패 책을 성공했을 경우에는 캐릭터 제공 하도록 수정

--- src/architecture/repositories/record.repository.ts
+++ src/architecture/repositories/record.repository.ts
@@ -155,6 +155,7 @@ class RecordRepository {
             where: {
                 userId,
                 bookId,
+                status: "success",
             },
             raw: true,
         });

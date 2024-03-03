75315c2c9e4f259a4589f3cf4fe0a4370841c3f0
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2024-03-03 21:07:54 +0900

    Update google_sheets.js

--- src/util/googleSheet/google_sheets.js
+++ src/util/googleSheet/google_sheets.js
@@ -1,81 +1,81 @@
-import { google } from "googleapis";
-import {
-    getFailedPlanPerPlan,
-    getPlanByTodayNewUser,
-    getPlanSuccessPerPlanCreate,
-    getRecordPerInProgressPlan,
-    getTodayDAU,
-    getTodayNewUser,
-    getTodayTouchPerCreatePlan,
-} from "./getData";
-import getDateFormat from "../setDateFormat";
+// import { google } from "googleapis";
+// import {
+//     getFailedPlanPerPlan,
+//     getPlanByTodayNewUser,
+//     getPlanSuccessPerPlanCreate,
+//     getRecordPerInProgressPlan,
+//     getTodayDAU,
+//     getTodayNewUser,
+//     getTodayTouchPerCreatePlan,
+// } from "./getData";
+// import getDateFormat from "../setDateFormat";
 
 export default async function data() {
     try {
-        const client = new google.auth.JWT(
-            process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            null,
-            process.env.GOOGLE_PRIVATE_KEY,
-            ["https://www.googleapis.com/auth/spreadsheets"],
-        );
-        client.authorize(function (err, tokens) {
-            if (err) {
-                console.log(err);
-                return;
-            } else {
-                updateData(client);
-            }
-        });
-        const newDate = new Date();
-        const today = getDateFormat(
-            new Date(newDate.setDate(newDate.getDate() - 1)),
-        );
+        // const client = new google.auth.JWT(
+        //     process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        //     null,
+        //     process.env.GOOGLE_PRIVATE_KEY,
+        //     ["https://www.googleapis.com/auth/spreadsheets"],
+        // );
+        // client.authorize(function (err, tokens) {
+        //     if (err) {
+        //         console.log(err);
+        //         return;
+        //     } else {
+        //         updateData(client);
+        //     }
+        // });
+        // const newDate = new Date();
+        // const today = getDateFormat(
+        //     new Date(newDate.setDate(newDate.getDate() - 1)),
+        // );
 
-        async function updateData(client) {
-            const sheets = google.sheets({ version: "v4", auth: client });
+        // async function updateData(client) {
+        //     const sheets = google.sheets({ version: "v4", auth: client });
 
-            const todayNewUser = await getTodayNewUser(today);
+        //     const todayNewUser = await getTodayNewUser(today);
 
-            const planSuccessPerPlanCreate =
-                (await getPlanSuccessPerPlanCreate()) + "%";
+        //     const planSuccessPerPlanCreate =
+        //         (await getPlanSuccessPerPlanCreate()) + "%";
 
-            const recordPerInProgressPlan =
-                (await getRecordPerInProgressPlan()) + "%";
-            const failedPlanPerPlan = (await getFailedPlanPerPlan(today)) + "%";
+        //     const recordPerInProgressPlan =
+        //         (await getRecordPerInProgressPlan()) + "%";
+        //     const failedPlanPerPlan = (await getFailedPlanPerPlan(today)) + "%";
 
-            const getDAU = await getTodayDAU(today);
+        //     const getDAU = await getTodayDAU(today);
 
-            const getTouchPerCreatePlan =
-                (await getTodayTouchPerCreatePlan(today)) + "%";
+        //     const getTouchPerCreatePlan =
+        //         (await getTodayTouchPerCreatePlan(today)) + "%";
 
-            const planByTodayNewUserPerTodayNewUser =
-                (todayNewUser === 0
-                    ? 0
-                    : Math.round(
-                          (await getPlanByTodayNewUser(today)) / todayNewUser,
-                      ) * 100
-                ).toFixed(2) + "%";
+        //     const planByTodayNewUserPerTodayNewUser =
+        //         (todayNewUser === 0
+        //             ? 0
+        //             : Math.round(
+        //                   (await getPlanByTodayNewUser(today)) / todayNewUser,
+        //               ) * 100
+        //         ).toFixed(2) + "%";
 
-            const todayData = new Array(
-                today,
-                getDAU,
-                getTouchPerCreatePlan,
-                recordPerInProgressPlan,
-                failedPlanPerPlan,
-                planSuccessPerPlanCreate,
-                todayNewUser,
-                planByTodayNewUserPerTodayNewUser,
-            );
+        //     const todayData = new Array(
+        //         today,
+        //         getDAU,
+        //         getTouchPerCreatePlan,
+        //         recordPerInProgressPlan,
+        //         failedPlanPerPlan,
+        //         planSuccessPerPlanCreate,
+        //         todayNewUser,
+        //         planByTodayNewUserPerTodayNewUser,
+        //     );
 
-            const request = {
-                spreadsheetId: process.env.GOOGLE_SHEETS_ID,
-                range: "시트1!A1",
-                valueInputOption: "USER_ENTERED",
-                insertDataOption: "OVERWRITE",
-                resource: { values: new Array(todayData) },
-            };
+        //     const request = {
+        //         spreadsheetId: process.env.GOOGLE_SHEETS_ID,
+        //         range: "시트1!A1",
+        //         valueInputOption: "USER_ENTERED",
+        //         insertDataOption: "OVERWRITE",
+        //         resource: { values: new Array(todayData) },
+        //     };
 
-            await sheets.spreadsheets.values.append(request);
+        //     await sheets.spreadsheets.values.append(request);
 
             return;
         }
